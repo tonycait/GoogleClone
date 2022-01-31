b1d653f790898c05bb19cc28f1d3244dab97d1e5
Antonio Iturriaga <70983411+tonycait@users.noreply.github.com>
2022-01-31 01:22:24 -0600

    ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Images from './img/g.png';
+import Dots from './img/dots.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <nav className="navbar">
+        <ul className="navelements">
+          <li>Gmail</li>
+          <li>Images</li>
+          <li className="btndotscontainer">
+            <img className="dots" src={Dots} alt="" />
+          </li>
+          <li><button>Login</button></li>
+        </ul>
+      </nav>
+
+      <div className="googlecontainer">
+        <img className="google" src={Images} alt="" />
+      </div>
+
+
+      <div className="searchcontainer">
+        <input type="search" name="" id="searchgoogle" placeholder="Search" />
+      </div>
+
+
+      
+
+      <div className="btncontainerarea">
+            <div className="buttoncontainer">
+                <button>Google Search</button>
+                <button>I'm Feeling Lucky</button>
+            </div>
+      </div>
+
+
+      <div className="linkcontainer">
+        <p>Google offered in: </p> <a href="#">English (United States</a>
+      </div>
+
+
+
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,49 @@
+
+
+import './Footer.css';
+
+function Footer() {
+    return (
+      <div className="Footer">
+
+          
+
+          <footer>
+              <div className="left">
+                  <ul>
+                      <li>About</li>
+                      <li>Advertising</li>
+                      <li>Business</li>
+                      <li>How Search Works</li>
+                  </ul>
+              </div>
+
+
+              <div className="right">
+                  <ul>
+                      <li>Privacy</li>
+                      <li>Terms</li>
+                      <li>Settings</li>
+                  </ul>
+              </div>
+          </footer>
+  
+  
+        
+  
+  
+  
+      </div>
+    );
+  }
+  
+  export default Footer;
+
+
+
+
+
+
+
+
+
